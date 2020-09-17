e37d4059423be6bfb5f180764960ea32b3e1d467
raphattack <raphnguyen@gmail.com>
2020-09-17 13:22:15 -0700

    Added logic to get matchup_type from form.

--- espn_ffb/views/standings.py
+++ espn_ffb/views/standings.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, current_app, render_template
+from flask import Blueprint, current_app, render_template, request
+from espn_ffb import util
 
 standings = Blueprint("standings", __name__, template_folder="templates")
 TABLE_HEADERS = ["Name", "Wins", "Losses", "Ties", "Win Percentage", "Points For", "Points Against", "Average Points For",
@@ -16,14 +17,16 @@ def show(year: str):
     # return "<h1 style='color:blue'>Hello There!</h1>" + str(sys.argv[1])
     query = current_app.config.get("QUERY")
 
+    matchup_type = request.args.get('matchup_type', default=util.REGULAR_SEASON)
     owner_id_to_name = get_owner_id_to_name(query)
     sacko = query.get_sacko_current()
+    is_playoffs = util.get_is_playoffs(matchup_type=matchup_type)
 
     if year == "overall":
-        records = query.get_standings()
+        records = query.get_standings(is_playoffs=is_playoffs)
     else:
-        records = query.get_standings(year=int(year))
+        records = query.get_standings(year=int(year), is_playoffs=is_playoffs)
 
-    return render_template('standings.html', title='Standings', table_headers=TABLE_HEADERS,
-                           column_names=COLUMN_NAMES, records=records, sacko=sacko,
-                           owner_id_to_name=owner_id_to_name)
+    return render_template('standings.html', title='Standings', table_headers=TABLE_HEADERS, column_names=COLUMN_NAMES,
+                           records=records, sacko=sacko, owner_id_to_name=owner_id_to_name, matchup_type=matchup_type,
+                           year=year)
