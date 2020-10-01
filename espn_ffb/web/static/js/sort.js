9703cd696b63e043313fa9c7cb857fed1ae9dc02
raphattack <raphnguyen@gmail.com>
2020-10-01 14:10:24 -0700

    Disabled buttons and info.

--- espn_ffb/web/static/js/sort.js
+++ espn_ffb/web/static/js/sort.js
@@ -2,17 +2,18 @@ $(document).ready(function() {
     var table = $('table.table').DataTable( {
         paging: false,
         searching: false,
-        buttons: {
-            buttons: [
-                { extend: 'copy', className: 'btn-outline-secondary btn-sm ml-2' },
-                { extend: 'excel', className: 'btn-outline-secondary btn-sm' },
-                { extend: 'pdf', className: 'btn-outline-secondary btn-sm' },
-                { extend: 'csv', className: 'btn-outline-secondary btn-sm' },
-                { extend: 'print', className: 'btn-outline-secondary btn-sm' },
-                { extend: 'colvis', className: 'btn-outline-secondary btn-sm' }
-            ],
-            dom: { button: { className: 'btn' } }
-        }
+        info: false
+        // buttons: {
+        //     buttons: [
+        //         { extend: 'copy', className: 'btn-outline-secondary btn-sm ml-2' },
+        //         { extend: 'excel', className: 'btn-outline-secondary btn-sm' },
+        //         { extend: 'pdf', className: 'btn-outline-secondary btn-sm' },
+        //         { extend: 'csv', className: 'btn-outline-secondary btn-sm' },
+        //         { extend: 'print', className: 'btn-outline-secondary btn-sm' },
+        //         { extend: 'colvis', className: 'btn-outline-secondary btn-sm' }
+        //     ],
+        //     dom: { button: { className: 'btn' } }
+        // }
     } );
     table.buttons().container().appendTo('.dataTables_wrapper .col-md-6:eq(0)');
 } );
